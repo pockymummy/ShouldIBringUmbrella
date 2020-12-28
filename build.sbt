ff227a6964c8f03f352a4cb9ace32959bb125531
ttangpornpis <thanawat.tangpornpisit@agoda.com>
2020-12-28 17:23:24 +0700

    prototype call weather api by sttp

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "ShouldIBringUmbrella"
 version := "0.1"
 
 scalaVersion := "2.13.4"
+
+libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % "3.0.0-RC13"
+
---
+++ src/main/scala/callWeather.scala
@@ -0,0 +1,10 @@
+import sttp.client3._
+
+object callWeather extends App {
+  val parameters = Map("lat" -> "13.745890", "lon" -> "100.538807", "exclude" -> "daily,minute", "appid" -> "739c8ef7068e5b553b1e08219ce008d4")
+  val myRequest = basicRequest
+    .get(uri"https://api.openweathermap.org/data/2.5/onecall?$parameters")
+  val backend = HttpURLConnectionBackend()
+  val response = myRequest.send(backend)
+  println(response.body)
+}
