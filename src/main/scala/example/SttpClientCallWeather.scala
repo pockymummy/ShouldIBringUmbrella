854e1218416b498fc90205751604b083431d92be
ttangpornpis <thanawat.tangpornpisit@agoda.com>
2021-01-10 12:20:27 +0700

    move test sttp client to example

--- src/main/scala/example/SttpClientCallWeather.scala
+++ src/main/scala/example/SttpClientCallWeather.scala
@@ -1,6 +1,8 @@
-import sttp.client3._
+package example
 
-object callWeather extends App {
+import sttp.client3.{HttpURLConnectionBackend, UriContext, basicRequest}
+
+object SttpClientCallWeather extends App {
   val parameters = Map("lat" -> "13.745890", "lon" -> "100.538807", "exclude" -> "daily,minute", "appid" -> "739c8ef7068e5b553b1e08219ce008d4")
   val myRequest = basicRequest
     .get(uri"https://api.openweathermap.org/data/2.5/onecall?$parameters")
